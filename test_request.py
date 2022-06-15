a00596441508f84c10ee2c35056ecfec017113fc
shearman <shearman@pop-os.localdomain>
2022-06-15 11:41:45 +0800

    new

---
+++ test_request.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from typing import List, Dict
+import pandas as pd
+
+def predict_templates(dataset: Dict):
+    response = requests.post('http://0.0.0.0:8080/single_inference', json = dataset)
+    #ipdb.set_trace()
+    print([doc for doc in response.iter_lines()])
+    response = response.json()
+    return response
+
+
+if __name__ == '__main__':
+    # dataset = {"sentence":"Apple computers"}
+    dataset = {
+                "context_left": "".lower(),
+                "mention": "Shakespeare".lower(),
+                "context_right": "'s account of the Roman general Julius Caesar's murder by his friend Brutus is a meditation on duty.".lower(),
+              }
+    entity_linking_df = pd.read_csv("data/entity_linking.csv")
+
+    df_json = entity_linking_df.to_json(orient="records")
+    df_json = json.loads(df_json)
+    # results = predict_templates(df_json)
+    results = predict_templates(dataset)
+    print(results)
